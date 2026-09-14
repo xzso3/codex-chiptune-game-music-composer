2a3ba30d4ab1c0a3cffca34d0ce923bc765d3911
Const <xzso3@outlook.com>
2026-09-15 01:49:18 +0800

    feat: add deterministic seeded random source

---
+++ src/random.ts
@@ -0,0 +1,24 @@
+export class SeededRandom {
+  private state: number;
+
+  constructor(seed: number) {
+    this.state = seed >>> 0 || 1;
+  }
+
+  next(): number {
+    let x = this.state;
+    x ^= x << 13;
+    x ^= x >>> 17;
+    x ^= x << 5;
+    this.state = x >>> 0;
+    return this.state / 0xffffffff;
+  }
+
+  int(min: number, max: number): number {
+    return Math.floor(this.next() * (max - min + 1)) + min;
+  }
+
+  pick<T>(items: readonly T[]): T {
+    return items[Math.floor(this.next() * items.length)]!;
+  }
+}
