27d1f6b8fce50d5748e23487b87779d805e99950
Const <xzso3@outlook.com>
2026-09-15 01:51:54 +0800

    feat: expose public composer API

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export * from "./types.js";
+export * from "./compose.js";
+export * from "./validate.js";
+export * from "./revise.js";
+export * from "./exportMidi.js";
+export * from "./exportBeepBox.js";
+export * from "./renderWav.js";
